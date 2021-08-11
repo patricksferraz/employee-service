6e92558b950ecc195f716e5a1dfb5fd4c2fa6bd5
patricksferraz <patrick.ferraz@outlook.com>
2021-08-11 19:02:25 -0300

    style: adds container kafka and changes topic name

--- infrastructure/external/topic/topic.go
+++ infrastructure/external/topic/topic.go
@@ -1,6 +1,6 @@
 package topic
 
 const (
-	NEW_EMPLOYEE    string = "new-employee"
-	UPDATE_EMPLOYEE string = "update-employee"
+	NEW_EMPLOYEE    string = "NEW_EMPLOYEE"
+	UPDATE_EMPLOYEE string = "UPDATE_EMPLOYEE"
 )
